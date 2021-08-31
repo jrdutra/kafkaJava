76a5db4428bac631a89f53e2e882445723ec5aa4
João Dutra <joao.core.dutra@everis.nettdata.com>
2021-08-30 21:17:15 -0300

    Upload

---
+++ src/apachekafka/EnvioKafka.java
@@ -0,0 +1,28 @@
+package apachekafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class EnvioKafka {
+
+	public static void main(String[] args) {
+
+		Properties properties = new Properties();
+
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localHost:9092");
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+		ProducerRecord<String, String> record = new ProducerRecord<>("testejava", "Olá Mundo!");
+		producer.send(record);
+		producer.close();
+
+	}
+
+}
